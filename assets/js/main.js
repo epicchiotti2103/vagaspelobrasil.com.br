ceb6fb5272f1c70cb93553e5f737705f97b778f6
Elio Picchiotti <eliosp20048@gmail.com>
2026-03-20 18:39:44 -0300

    feat: estrutura inicial vagaspelobrasil

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/* ================================================
+   VAGAS PELO BRASIL — main.js
+   PostHog (via proxy) + mobile nav
+   ================================================ */
+
+// ---------- Mobile Nav Toggle ----------
+(function() {
+  var toggle = document.getElementById('navToggle');
+  var links = document.getElementById('navLinks');
+  if (toggle && links) {
+    toggle.addEventListener('click', function() {
+      links.classList.toggle('open');
+    });
+  }
+})();
+
+// ---------- PostHog ----------
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onFeatureFlags onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+
+posthog.init('phc_XXXXXXXXXXXXXXXXXXXXXXXXXXXXX', {
+  api_host: 'https://ph.vagaspelobrasil.com.br',
+  person_profiles: 'identified_only',
+  capture_pageview: true
+});
